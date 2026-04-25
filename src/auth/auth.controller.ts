d5d662362d99b73947198ad2e53ce0e66ab65763
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2026-04-25 15:58:30 +0530

    feat: add global auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -10,11 +10,13 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { RefreshAuthGuard } from './guards/refresh-auth.guard';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { Public } from './decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) { }
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @UseGuards(LocalAuthGuard)
   @Post('login')
@@ -22,6 +24,7 @@ export class AuthController {
     return this.authService.login(req.user.id);
   }
 
+  @Public()
   @UseGuards(RefreshAuthGuard)
   @Post('refresh')
   async refreshToken(@Request() req) {
