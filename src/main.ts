5a80a59593c42de591590deb7c23cf71857f80de
Syed Md Mihan Chistie <tubbachistie@gmail.com>
2026-04-26 21:43:22 +0530

    fix: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { urlencoded, json } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
+  app.use(json({ limit: '5mb' }));
+  app.use(urlencoded({ extended: true, limit: '5mb' }));
+
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
+
